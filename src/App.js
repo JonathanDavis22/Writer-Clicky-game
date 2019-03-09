e4de367258ee940a20d9562a27553d30cb7ad2d5
jon91@utexas.edu <jon91@utexas.edu>
2019-03-08 23:32:18 -0800

    Bug check

--- src/App.js
+++ src/App.js
@@ -60,7 +60,7 @@ class App extends Component {
       this.state.cards.forEach(card => {
         card.count = 0;
       });
-      alert(`Game Over :( \nscore: ${this.state.score}`);
+      alert(`Game Over :( \nScore: ${this.state.score}`);
       this.setState({score: 0});
       return true;
     }
